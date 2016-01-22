188b1cd982ad326b925967ec423a5b92c08d3e4f
otakuto <otakuto893@gmail.com>
2016-01-22 17:59:39 +0900

    fix

--- gf.hs
+++ gf.hs
@@ -1,15 +1,14 @@
 import System.Environment
 import Codec.Picture
-
-gf :: Int -> [[Int]]
-gf n = map (\x -> (map (g n x) [0..(n-1)])) [0..(n-1)]
+import Debug.Trace
 
 g :: Int -> Int -> Int -> Int
 g n x y = mod (x * y) n
 main = do
   args <- getArgs
   let n = read $ args !! 0 :: Int
-  let red x y = truncate (((fromIntegral ((gf n) !! y !! x)) / fromIntegral n) * 255)
+  let red x y = truncate (((fromIntegral (g n x y)) / fromIntegral n) * 255)
   let blue x y = 255 - (red x y)
   let render x y = PixelRGB8 (fromIntegral (red x y)) 0 (fromIntegral (blue x y))
-  writePng ("gf" ++ (show n) ++ ".png") $ generateImage render n n
+  let rendertrace x y = if x == 0 then trace (show y) render x y else render x y
+  writePng ("gf" ++ (show n) ++ ".png") $ generateImage rendertrace n n
